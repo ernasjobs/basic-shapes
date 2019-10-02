9bee8c267a912d6b88c9b5c4c3a14d05d9ec34bf
Ernas Pikuli <ernasjobs@gmail.com>
2019-10-02 20:01:29 +0100

    Create 3 rectangle object, passed the class fields into a constructor and call the method GetArea()

--- Program.cs
+++ Program.cs
@@ -6,7 +6,15 @@ namespace basic_shapes
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Rectangle r1=new Rectangle(34,25);
+            Rectangle r2=new Rectangle(10,5);
+            Rectangle r3=new Rectangle(15,30);
+
+            Console.WriteLine("The are of first rectangle is: {0}",r1.GetArea());
+            Console.WriteLine("The are of second rectangle is: {0}",r2.GetArea());
+            Console.WriteLine("The are of third rectangle is: {0}",r3.GetArea());
+
+            Console.ReadKey(true);
         }
     }
 }
