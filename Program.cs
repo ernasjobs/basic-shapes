e75676c9b355ab2b7c58deb50b928060dc527369
Ernas Pikuli <ernasjobs@gmail.com>
2019-10-02 20:03:04 +0100

    Final version of app

--- Program.cs
+++ Program.cs
@@ -10,9 +10,9 @@ namespace basic_shapes
             Rectangle r2=new Rectangle(10,5);
             Rectangle r3=new Rectangle(15,30);
 
-            Console.WriteLine("The are of first rectangle is: {0}",r1.GetArea());
-            Console.WriteLine("The are of second rectangle is: {0}",r2.GetArea());
-            Console.WriteLine("The are of third rectangle is: {0}",r3.GetArea());
+            Console.WriteLine("The area of first rectangle is: {0}",r1.GetArea());
+            Console.WriteLine("The area of second rectangle is: {0}",r2.GetArea());
+            Console.WriteLine("The area of third rectangle is: {0}",r3.GetArea());
 
             Console.ReadKey(true);
         }
